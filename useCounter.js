02d91269aab830ed3a07607b6a934c85c08527ea
Julen Castillo <julencas.desarrollo@gmail.com>
2022-11-30 15:43:18 +0100

    first commit util hooks

---
+++ useCounter.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export const useCounter = (initialValue = 1) => {
+    const [counter, setCounter] = useState(initialValue);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    };
+
+    const decrement = () => {
+        console.log(counter);
+        return counter > 1 ? setCounter(counter - 1) : null;
+    };
+
+    const reset = () => {
+        setCounter(initialValue);
+    };
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset,
+    };
+};
